5da05af86e5d3631303339d86e4ae232f093b691
nicholasfb <nicholasbarbosads2001@gmail.com>
2024-04-25 19:17:41 -0300

    Criando branch Pessoa e classe Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        if(idade < 0) {
+            System.out.println("Idade deve ser maior que zero");
+        }
+    }
+}
